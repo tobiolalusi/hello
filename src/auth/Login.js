35c91095767e0a1fbe1ed4d40323769ba4379f73
Oludayo Fayemi <fayemi.oludayo@gmail.com>
2020-12-22 01:09:01 +0100

    login

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import  {useEffect, useState} from 'react';
+import { CLIENT_ID, CLIENT_SECRET, SCOPES, CALLBACK_URL } from './constants';
 
 const Login = () => {
+
+	const scopes = SCOPES.join(" ");
+
+	const logintoApi = () => {
+		fetch("https://accounts.spotify.com/authorize"+
+		'?response_type=code' +
+		'&client_id=' + CLIENT_ID +
+		(scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+		'&redirect_uri=' + encodeURIComponent(CALLBACK_URL))
+		.then()
+	};
+
 	return (
-		<button>
+		<button >
 			Log in
 		</button>
 	)
---
+++ src/auth/constants.js
@@ -0,0 +1,5 @@
+
+export const CLIENT_ID = "823dff95fa5744cf9cb9f4ee59db05c2";
+export const CLIENT_SECRET = " 7a62e04456bd446fb63cbf89b42ad918";
+export const CALLBACK_URL = "http://localhost:8080/login/callback";
+export const SCOPES = ["user-read-private ", "user-read-email"];
