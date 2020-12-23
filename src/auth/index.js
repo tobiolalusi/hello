124582336df774f6d18f6fc48beba769c46f1f6a
Oluwatobiloba Olalusi <hello@tobiolalusi.com>
2020-12-23 04:20:28 +0100

    updated gitignore

---
+++ src/auth/index.js
@@ -0,0 +1,5 @@
+import Callback from './Callback'
+
+export {
+	Callback
+}
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = ({username}) => {
+    return (
+        <div className='px-8 py-4 flex justify-between text-white'>
+            <img className='h-10' src='/spotify_logo.png' alt='Spotify logo' />
+            <h4>{`Wazzup,  ${username}`}</h4>
+        </div>
+    );
+};
+
+export default Header
